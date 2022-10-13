0154e8d4d3d90940470fb4c855ed8759fd90341a
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-13 09:30:51 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,1 +1,17 @@
-package
+/*
+ * Realiza un programa que pida dos números y que luego muestre el resultado de su multiplicación
+ * Author: Pablo Camino Vázquez
+ */
+
+import java.util.Scanner;
+public class Ejercicio1{
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el primer numero: ");
+        int num1 = sc.nextInt();
+        System.out.println("Introduzca el segundo numero: ");
+        int num2 = sc.nextInt();
+        System.out.println("El resultado de "+num1+"x"+num2+" = "+(num1*num2));
+
+    }
+}
