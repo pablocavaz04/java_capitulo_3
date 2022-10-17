0d5956a4ed5c74d699fd72111ba64464e7053f42
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-17 08:45:10 +0200

    fin

---
+++ Ejercicio9.java
@@ -0,0 +1,15 @@
+/*
+ * Escribe un programa que calcule el volumen de un cono según la fórmula V =1/3 πr2h
+ */
+import java.util.Scanner;
+public class Ejercicio9 {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el radio del cono: ");
+        double radio = sc.nextDouble();
+        System.out.println("Introduzca la altura del cono: ");
+        double h = sc.nextDouble();
+        System.out.println("El volumen de este cono es de : "+(Math.PI*(Math.pow(radio, 2)*h))/3+" in");
+
+    }
+}
